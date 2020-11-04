32afe43106477a9480d8285a9e37292c700f353d
rosarioconde <conde.rosario88@gmail.com>
2020-11-04 17:10:26 -0300

    schemaLogin  + validaciones / post login

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,27 @@ const schemaRegister = Joi.object({
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required()
 })
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/login', async (req, res) => {
+   
+    const { error } = schemaLogin.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message })
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ error: 'Usuario no encontrado' });
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).json({ error: 'password no válida' })
+
+    res.json({
+        error: null,
+        data: 'bienvenido'
+    })
+})
 
 router.post('/register', async (req, res) => {
 
