4e105ad4dbec43973af9fe623d4f68472dd5a9be
rconde <rconde.mprod@gmail.com>
2020-11-03 21:45:16 -0300

    body routes inicio server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const app = express();
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+
+// import routes
+
+// route middlewares
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'ok!'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor en: ${PORT}`)
+})
