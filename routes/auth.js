3ee3135d8e8249b170022674ec680f9aca3f04a1
rosarioconde <conde.rosario88@gmail.com>
2020-11-04 16:49:29 -0300

    validaciones + email unico

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Joi = require('@hapi/joi');
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
 
 router.post('/register', async (req, res) => {
-   
+
+    const { error } = schemaRegister.validate(req.body)
+
+    if (error) {
+        return res.status(400).json(
+            { error: error.details[0].message }
+        )
+    }
+
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    if (isEmailExist) {
+        return res.status(400).json(
+            { error: 'Email ya registrado' }
+        )
+    }
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
